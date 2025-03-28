ad8b28da3fe160959018aaccae23d08e165dc076
Rahul00q <rm9691061256@gmail.com>
2025-03-28 17:32:33 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,10 @@
+// js 1995 devloped and mr. brandon eich and there are created in only 10 days;
+//  name =mocha ;
+// ecma script registred name  (european computer manufacture association);
+// js is a dynamiclly type language;
+// js is a client side and server side scripting language;
+// js is very light weighted language ;
+// it is mostly famous in 2014-15 in ecma 6 update ;
+// it is firstly used in server site in 2008 ;
+//  latest version in js ecma6 findLast, and findLastIndex methods on Array.prototype and TypedArray.prototypet 
+// The newest version of JavaScript is called ECMAScript 2023 or ES2023 for short. It came out in June 2023
---
+++ first1.js
@@ -0,0 +1,53 @@
+// js 1995 devloped and mr brandon eich and there are created in only 10 days;
+//  name =mocha ;
+
+// js dynamiclly type language hai , js scriped language , and js ek light weight language hai;
+// console.log are using to print the elements;
+
+// console.log("rahul");
+// console.log("meena");
+// console.log("i love js");
+
+
+
+// variables in js
+// variables are containers for data 
+// age = 24;
+// name = "ram" ;
+// console.log(name);
+name = "raju";
+age = 25;
+price= 99.09;
+radius =14;
+x = null;
+y = undefined;
+isFollow = true;
+console.log(y);
+
+
+let name = "rahul";
+let price =  200;
+let age= 16;
+age = 56;
+age = 45;
+
+console.log(fullName);
+
+// const ki value ko fir se change nahi kr skte hain update bhi nahi kr skte hain;
+ const a= 10;
+ const b= 10;
+//  {iske andr jo code likha jata hai use hme ek blockbiolte hain}
+{
+    let a = 5;
+    console.log(a);
+}
+{
+let a = 45;
+console.log(a);
+}
+
+
+// DATA TYPE IN JAVA ScriptProcessorNode;
+// Number , String , undefined , null , BigInt, Symbol
+
+
