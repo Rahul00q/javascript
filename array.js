3400c8872a90f1cc1ef8dccca181c7518e412eb6
Rahul00q <rm9691061256@gmail.com>
2025-04-06 23:41:34 +0530

    update

--- array.js
+++ array.js
@@ -4,9 +4,9 @@
  //  3. push() = add a new element to the end;
  //  4. shift() = remove the frist elements;
  //  5. unshift()= add a new element to the beginning; 
- //  6. slice() =
- //  7. concAt() =
- //  8. spslice() =
+ //  6. slice() =   tokde me tod  kr deta hai slice me  and slice are gives of an array ;
+ //  7. concAt() = add krta hai do string ko
+ //  8. spslice() =  from where to add , how array to delete , what to add;
  // let fruits=["Apple","Mango","Banana","Grapes"]    //  Array is nonpremitive;
 //  console.log(fruits[2]);    // to access array elements ;
 //  console.log(fruits.length); // to take length;
@@ -22,6 +22,33 @@
     // fruits.unshift("aaa");   add a new elements to the beginning and frist;
     // console.log(fruits);
 
-let a= ["rahul", "meena"];
-let b= ["prem", "kumar"];
-console.log(a.concat(b));
+// let a= ["rahul", "meena"];
+// let b= ["prem", "kumar"];
+// console.log(a.concat(b));
+
+//   ---------------------------------------Day-2------------------------------------------------------------------
+
+// slice() method tokde me tod deta hai;
+// let fruits=["Apple","Mango","Banana","Grapes"];
+// console.log(fruits.slice(1,3));
+// let fruits=["Apple","Mango","Banana","Grapes"];
+// fruits.splice(1,0,"ganne ka juice");   // from where to add , how array to delete , what to add;
+// console.log(fruits);
+
+
+//  ------------------------------sort()--------------------------------------------------
+// sort are using to accending order to use sort method
+// let arr=[2,6,3,7,1,8];
+// console.log(arr.sort());
+
+// ------------------------------------reverse()-----------------------------------------------
+// reverse method are using to decending order using to reverse methods;
+// let arr=[2,6,3,7,1,8];
+// console.log(arr.reverse());
+
+
+
+let arr=[2,6,3,7,1,8];
+console.log(arr.sort());
+console.log(arr.reverse());
+extends;
--- first.js
+++ first.js
@@ -4,12 +4,13 @@
 // js is a dynamiclly type language;
 // js is a client side and server side scripting language;
 // js is very light weighted language ;
-// it is mostly famous in 2014-15 in ecma 6 update ;
-// it is firstly used in server site in 2008 nodejs language in used ;
+// it is mostly famous in 2014-15 in ecma es6 update ;
+// it is firstly used in server site in 2008-09 nodejs language in used ;
 // The newest version of JavaScript is called ECMAScript 2024 or 15th ES2024 for short. It came out in June 2024
 
 
- // 'use strict' 2016 
+ // 'use strict' 2016 ;
+ // let and conts, arrow function;
 // Data Type ;
 // Three two of data types 
 //1. primitive data types
@@ -22,32 +23,73 @@
 // Number , String , undefined , null , BigInt, Symbol;
 // var , let,  keyword ;
 
+// Scope 
+//  scope are use in the curly brases{};
+// scope refers to the accessibility of variables , function ,and objects 
+
+
+// VAR are function scope;
+// LET or CONS are blocked scope;
+
+// HOSTING;
+// 
+// redecleration=var
+// reassignment = var or let
+
+// **Hoisting
+// jitna bhe var key word sa variable decliration ha wo unka function ka andae top pa ja ka baith jata ha
+// Hoisting are use only var
+// its is donot work in lets and cons
 
 
 
+// ** Java Script ia a synchronus is is works in fifo or Quee
+// ** function are most use in componey are arrow function
+// ** two key are lounched in 2015 es6 update let, const;
+// ** Why lets const are launched 
+
+// HW
+// find es current using update or version 
+// ans= ECMAScript 2024 or 15 th
+
+// hw
+// Scope,Hosting 
+
+// * Scope
+// Scope are use in the curly brases {}
+//  scope refers to the accessibility of variables, functions, and objects in different parts of your code
+
+// VAR are function scope
+  
+// let or cons are blocked scope
 
 
 
+// *BOM
+// Browser Object Model
 
+// `.Intrecting with the browsing window
 
+// first we write window.alert("hi")
+// method; 
+//  alert = msg show krta hai; confirm = do buttons deta hai, true /false; prompt = input lene ke liye and feedback lene ke liye;
 
+// hw 
+// user sa prompt ma do value mangane ha 
+// int no 1
+// int no 2
 
+// then add the ans
 
+//  ** parseInt ,parseFloat ,Number it is convet string into number
 
+// **DOM
+//   document object model
 
+// hw
+// 2 ya 3 button ushka click karna pa alert aiya this is 1 fn 2
 
-
-
-
-
-
-
-
-
-
-
-
-
+// 3function 3 button
 
 // string methods ;
 // length() , slice(), concat(), substring(), replace(), toUpper(), replace(), charAt(), split(), indexOff() ,trim();
