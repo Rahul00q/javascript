dbaf36395a56b51329d500d33bee6069e6b8dba9
Rahul00q <rm9691061256@gmail.com>
2025-04-15 17:46:28 +0530

    update

--- object.js
+++ object.js
@@ -1,12 +1,17 @@
 // key or pairs value
 
-// key value ;
+// key:value ;
  let obj ={ 
 
    Name:"Rahul",
    Age: 25,
    City: "Bhopal"
+   // key hmesha uniq honi chiye;
 
 
  }
- console.log(obj);
+ console.log(obj);   // to print full object;
+ console.log(obj.Age);  // to access object values;
+
+ 
+ 
