4696a3e22526a531c7eb031965915f28fa0e689b
Rahul00q <rm9691061256@gmail.com>
2025-04-25 19:38:34 +0530

    update

---
+++ localstorage.js
@@ -0,0 +1,38 @@
+// let sendData = () => {
+
+//     localStorage.setItem("Name", "Rahul")
+
+//     localStorage.setItem("Age", "23")
+//     //  localStorage.setItem("number","9796959644")
+
+//     localStorage.removeItem("number", "9796959644")
+
+//     location.reload()
+// }
+
+// let Show = document.querySelector("#show")
+// let username  = localStorage.getItem("Name")
+// let userage = localStorage.getItem("Age")
+// Show.innerHTML = `Welcome ${username} and Your Age is ${userage} `
+
+
+// let removeData =()=>{
+//     // localStorage.removeItem("Name")     // one items delete;
+//     localStorage.clear()                  // all data is remove;
+
+
+//     location.reload()
+// }
+
+
+
+
+    
+
+
+
+
+
+
+
+
